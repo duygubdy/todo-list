8a8f0aec90ae23941b2ede2f021e7866e67a5ab3
duygubdy <duygubdy@gmail.com>
2022-07-12 15:12:34 +0300

    Add button icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
 import { useState } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
+import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 function Todo({text,todo,todos,setTodos,date1,date2}) {
   
@@ -10,7 +14,7 @@ function Todo({text,todo,todos,setTodos,date1,date2}) {
     done:"done"
   }
   const [status,setStatus]=useState(Status.notStarted)
-
+  const [editable,setEditable]=useState(false)
   const removeHandler=()=>{
     setTodos(todos.filter(el=>el.id !==todo.id))
   }
@@ -20,19 +24,24 @@ function Todo({text,todo,todos,setTodos,date1,date2}) {
     <div className='to-do'>
       <li className='todoItems'>{text + ' ' + date1 + ' ' + date2  +status } </li>
       
-      <button
+      <PlayArrowIcon
+        color="success"
         className='start-button'
         name='start'
         onClick={()=>setStatus(Status.process)}
-        >start</button>
+        />
       
-      <button 
+      <CheckCircleOutlineRoundedIcon
         className='done-button'
         name='done'
         onClick={()=>setStatus(Status.done)}
-        >done</button>
+        />
 
-      <button onClick={removeHandler}>delete</button>
+      <DeleteOutlineRoundedIcon onClick={removeHandler}/>
+
+      <EditIcon/>
+      
+      
       
     </div>
     
